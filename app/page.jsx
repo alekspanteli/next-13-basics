c675ed575e5f66713c02e654793fcb4d643ad659
alekspanteli <alekspanteli@rambler.ru>
2022-11-02 22:43:25 +0000

    fix container

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="xl:container">
+    <div className="md:container px-4">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
