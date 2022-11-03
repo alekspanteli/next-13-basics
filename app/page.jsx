72a6ad1323c0b135034b5b912859d7a49a4094e6
alekspanteli <alekspanteli@rambler.ru>
2022-11-03 22:15:22 +0000

    Update 9 files

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-3xl font-bold underline">Home page</h1>
+      <button className="btn">Test here</button>
     </div>
   );
 }
