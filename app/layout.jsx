effcb4152c6e08d9d6cee2455e0e11237cd43060
alekspanteli <alekspanteli@rambler.ru>
2022-11-03 21:49:45 +0000

    Update 5 files

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,14 +2,33 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html className="page" lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Document</title>
       </head>
-      <body>{children}</body>
+
+      <body className="page__body">
+        <header className="header">
+          <a className="header__logo">Logo</a>
+          <nav aria-label="Primary">
+            <ul className="menu">
+              <li className="menu__item">
+                <a className="menu__link">Home</a>
+              </li>
+              <li className="menu__item">
+                <a className="menu__link">About</a>
+              </li>
+              <li className="menu__item">
+                <a className="menu__link">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="md:container px-4">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!!!!!!!!!!!!!!!!!!!!
-      </h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold underline">Home page</h1>
     </div>
   );
 }
