a2cbbc67d5accd07113bf4fe9c330f947777ee93
alekspanteli <alekspanteli@rambler.ru>
2022-11-03 06:45:28 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="md:container px-4">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!!!!!!!!!!!!!!!!!!!!
+      </h1>
     </div>
   );
 }
